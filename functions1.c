51802f99cb0a040a8c9d2065a2d9c2240e1d4765
jomanabanana <jomanaehab666@yahoo.com>
2023-06-26 14:38:51 -0700

    done

--- functions1.c
+++ functions1.c
@@ -106,6 +106,7 @@ int print_hexadecimal(va_list types, char buffer[],
  * @size
  * return
  */
+
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
