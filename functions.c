317e200b52e462ee99b7559a560a46b27127e1a2
jomanabanana <jomanaehab666@yahoo.com>
2023-06-27 14:39:24 -0700

    done

--- functions.c
+++ functions.c
@@ -154,23 +154,23 @@ int print_int(va_list types, char buffer[],
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	unsigned int n, m, i, sum;
+	unsigned int n, n2, i, sum;
 	unsigned int a[32];
 	int countt;
 
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
-	UNUSED(size);
 	UNUSED(precision);
+	UNUSED(size);
 
 	n = va_arg(types, unsigned int);
-	m = 2147483648;
-	a[0] = n / m;
+	n2 = 2147483648;
+	a[0] = n / n2;
 	for (i = 1; i < 32; i++)
 	{
-		m /= 2;
-		a[i] = (n / m) % 2;
+		n2 /= 2;
+		a[i] = (n / n2) % 2;
 	}
 	for (i = 0, sum = 0, countt = 0; i < 32; i++)
 	{
