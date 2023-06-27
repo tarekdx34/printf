2392731a9c3e9437469acb3f668adb3d31d60d77
tarekdx34 <tarekdx3@gmail.com>
2023-06-27 15:22:21 -0700

    Revert "done"
    This reverts commit 38feb5c07318e58976a0198db968a253c9f730f0.

--- functions.c
+++ functions.c
@@ -156,7 +156,7 @@ int print_binary(va_list types, char buffer[],
 {
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
-	int count;
+	int countt;
 
 	UNUSED(buffer);
 	UNUSED(flags);
@@ -172,7 +172,7 @@ int print_binary(va_list types, char buffer[],
 		m /= 2;
 		a[i] = (n / m) % 2;
 	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	for (i = 0, sum = 0, countt = 0; i < 32; i++)
 	{
 		sum += a[i];
 		if (sum || i == 31)
@@ -180,8 +180,8 @@ int print_binary(va_list types, char buffer[],
 			char z = '0' + a[i];
 
 			write(1, &z, 1);
-			count++;
+			countt++;
 		}
 	}
-	return (count);
+	return (countt);
 }
